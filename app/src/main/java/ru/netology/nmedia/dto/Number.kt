f664e1690ddbc37f1fe7b03b6b07ee469841dc32
Nika Chekulaeva <nika.702@mail.ru>
2023-09-24 19:20:15 +0300

    2.1. Обработка событий в Android

---
+++ app/src/main/java/ru/netology/nmedia/dto/Number.kt
@@ -0,0 +1,36 @@
+package ru.netology.nmedia.dto
+
+class Number {
+    companion object {
+        fun setNumberView(number: Int): String {
+            return when (number) {
+                in 0..999 -> number.toString()
+                in 1000..9999 -> {
+                    val firstDigit = number.toString()[0]
+                    val secondDigit = number.toString()[1]
+
+                    if (secondDigit == '0') {
+                        firstDigit.toString() + "K"
+                    } else {
+                        firstDigit.toString() + "." + firstDigit + "K"
+                    }
+                }
+
+                in 10_000..99_000_000 -> {
+                    (number / 1000).toString() + "K"
+                }
+
+                else -> {
+                    val firstDigit = number / 1_000_000
+                    val secondDigit = (number / 100_000).toString().last()
+
+                    if (secondDigit == '0') {
+                        firstDigit.toString() + "K"
+                    } else {
+                        firstDigit.toString() + "." + secondDigit + "K"
+                    }
+                }
+            }
+        }
+    }
+}
