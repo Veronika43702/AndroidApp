8356c4578296f210fbf17d739ef63ec1fa4dbf34
Veronika Chekulaeva <nika.702@mail.ru>
2023-10-10 16:03:56 +0300

    second Try Edit

--- app/src/main/java/ru/netology/nmedia/activity/EditPostActivityContract.kt
+++ app/src/main/java/ru/netology/nmedia/activity/EditPostActivityContract.kt
@@ -3,16 +3,12 @@ package ru.netology.nmedia.activity
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContract
-import ru.netology.nmedia.R
-import ru.netology.nmedia.dto.Post
 
 class EditPostActivityContract : ActivityResultContract<String?, String?>() {
-    override fun createIntent(context: Context, input: String?) = Intent(context, EditPostActivity::class.java).apply{
-        putExtra(Intent.EXTRA_TEXT, input)
-    }
+    override fun createIntent(context: Context, input: String?) =
+        Intent(context, EditPostActivity::class.java).
+            putExtra(Intent.EXTRA_TEXT, input)
 
     override fun parseResult(resultCode: Int, intent: Intent?): String? =
         if (resultCode == Activity.RESULT_OK) {
